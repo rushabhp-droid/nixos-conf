79f84fbe48b14084b6ace157612ea091a2b7f50a
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-06-14 17:26:24 +0530

    Alacritty Configuration: Changed Font Size

--- hosts/laptop/home/modules/alacritty.nix
+++ hosts/laptop/home/modules/alacritty.nix
@@ -10,10 +10,10 @@
 
     settings = {
       font = {
+        size = 14.0;
         normal = {
           family = "JetBrainsMono Nerd Font";
         };
-        size = 14.0;
       };
     };
   };
