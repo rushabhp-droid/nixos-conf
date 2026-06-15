505c7234842b0ae0f95760335924e8704ba78089
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-06-15 18:11:34 +0530

    fix: update TLP power settings to improve battery efficiency and expand runtime PM denylist

--- modules/nixos/hardware-tweaks.nix
+++ modules/nixos/hardware-tweaks.nix
@@ -30,13 +30,13 @@
     services.tlp = {
       enable = true;
       settings = {
-        RUNTIME_PM_DENYLIST = "01:00.0";
+        RUNTIME_PM_DENYLIST = "01:00.0 01:00.1";
 
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
