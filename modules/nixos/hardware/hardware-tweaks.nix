c6efab199b68d20ce1ced15f81f7e9c1bf314550
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-06-16 13:22:26 +0530

    fix: adjust CPU energy policy and power limits for AC mode in hardware tweaks

--- modules/nixos/hardware/hardware-tweaks.nix
+++ modules/nixos/hardware/hardware-tweaks.nix
@@ -35,10 +35,10 @@
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
 
         CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MAX_PERF_ON_AC = 80;
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 20;
       };
