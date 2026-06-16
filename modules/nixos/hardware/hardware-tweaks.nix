3d369dd7981835e7b27bd1f68a5fae51e1fc1e48
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-06-16 19:34:51 +0530

    perf: set CPU scaling governor and max performance to maximum on AC power

--- modules/nixos/hardware/hardware-tweaks.nix
+++ modules/nixos/hardware/hardware-tweaks.nix
@@ -31,14 +31,14 @@
       settings = {
         # RUNTIME_PM_DENYLIST = "01:00.0 01:00.1"; # Removed: This prevents the NVIDIA GPU (PCI:1:0:0) from sleeping (D3cold)!
 
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
 
         CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 80;
+        CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 20;
       };
