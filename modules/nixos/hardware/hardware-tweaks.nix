33f02d97a353c8c1a7a507b1612ec587c44581da
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-06-18 13:55:05 +0530

    Migrate to PPD

--- modules/nixos/hardware/hardware-tweaks.nix
+++ modules/nixos/hardware/hardware-tweaks.nix
@@ -20,27 +20,8 @@
       # Proactive thermal management for Intel CPUs
       thermald.enable = true;
 
-      # TLP Power Management
-      tlp = {
-        enable = true;
-        settings = {
-          # RUNTIME_PM_DENYLIST = "01:00.0 01:00.1"; # Removed: This prevents the NVIDIA GPU (PCI:1:0:0) from sleeping (D3cold)!
-
-          # Force the NVIDIA GPU and its Audio device to 'auto' so finegrained works on AC too
-          RUNTIME_PM_ENABLE = "01:00.0 01:00.1";
-
-          CPU_SCALING_GOVERNOR_ON_AC = "performance";
-          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-          CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-
-          CPU_MIN_PERF_ON_AC = 0;
-          CPU_MAX_PERF_ON_AC = 100;
-          CPU_MIN_PERF_ON_BAT = 0;
-          CPU_MAX_PERF_ON_BAT = 20;
-        };
-      };
+      # Power Profiles Daemon Management
+      power-power-profiles-daemon.enable = true;
     };
 
     # Bluetooth
@@ -54,7 +35,7 @@
       };
     };
 
-    # TLP Power Management
+    # NixOS Power Management
     powerManagement.enable = true;
   };
 }
