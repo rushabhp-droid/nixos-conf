893c21a52e9ca18de3faf7f05d18d8fe415b2149
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-04 13:46:44 +0530

    fixed hyprland lua configuration syntax errors
    enable_swallow, swallow_regex are a part of misc subcategory.
    which were not defined correctly

--- modules/home-manager/desktop/hyprland/lua/misc.lua
+++ modules/home-manager/desktop/hyprland/lua/misc.lua
@@ -3,9 +3,9 @@ hl.config({
 		force_default_wallpaper = -1,  -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
 		vrr                     = 1,
+		enable_swallow          = true,
+		swallow_regex           = "^(kitty|Alacritty)$",
 	},
-	enable_swallow = true,
-	swallow_regex = "^(kitty|Alacritty)$",
 })
 
 -- Layout
