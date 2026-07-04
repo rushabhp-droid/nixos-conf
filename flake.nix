d2a00460dc492dc12a21ca7b5932a04cfb9181a4
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-04 13:08:43 +0530

    feat: add nixfmt-tree as the default nix formatter in flake.nix

--- flake.nix
+++ flake.nix
@@ -116,6 +116,8 @@
         };
     in
     {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
+
       nixosConfigurations = builtins.mapAttrs mkHost {
         netanyahu = {
           userName = "rushabhp";
