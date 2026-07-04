20fab1d816b1b415ef049ee9b673e0a262d96e3c
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-04 13:23:12 +0530

    add yt-dlp, aria2, ffmpeg package

--- modules/home-manager/programs/multimedia.nix
+++ modules/home-manager/programs/multimedia.nix
@@ -6,5 +6,24 @@
     shortwave
     loupe # image viewer
     clapper # Media Player
+    aria2
+    ffmpeg
   ];
+
+  programs = {
+    yt-dlp = {
+      enable = true;
+      settings = {
+        color = [
+          "stdout:no_color"
+          "stderr:always"
+        ];
+        downloader = "aria2c";
+        downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+        embed-subs = true;
+        embed-thumbnail = true;
+        sub-langs = "all";
+      };
+    };
+  };
 }
