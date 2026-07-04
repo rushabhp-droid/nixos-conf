31023a70f8b059e1b3c0e391d4d18adeef0d0e90
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-04 17:02:51 +0530

    feat: configure obsidian vaults with community plugins and custom themes via nix-managed settings

--- modules/home-manager/programs/obsidian.nix
+++ modules/home-manager/programs/obsidian.nix
@@ -1,7 +1,32 @@
-_: {
+{ config, pkgs, lib, ... }:
+let
+  colors = config.lib.stylix.colors.withHashtag;
+  fonts = config.stylix.fonts;
+  obsidian-plugins = import ../../../pkgs/obsidian { inherit pkgs lib; };
+in
+{
+  stylix.targets.obsidian.enable = false;
   programs.obsidian = {
     enable = true;
-    vaults."Documents/Notes".enable = true;
+    vaults."Documents/Notes" = {
+      enable = true;
+      settings = {
+        communityPlugins = with obsidian-plugins; [
+          dataview
+          templater
+          calendar
+          tasks
+          style-settings
+        ];
+        themes = with obsidian-plugins; [
+          ayu-theme
+        ];
+        app = {
+          theme = "obsidian";
+          cssTheme = "obsidian-ayu-theme";
+        };
+      };
+    };
     defaultSettings = {
       app = {
         defaultViewMode = "preview";
