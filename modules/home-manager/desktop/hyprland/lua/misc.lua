7dc898877c49642ca90dab0703d2f094c74780b9
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-04 13:39:54 +0530

    format and enable window swallowing

--- modules/home-manager/desktop/hyprland/lua/misc.lua
+++ modules/home-manager/desktop/hyprland/lua/misc.lua
@@ -1,20 +1,22 @@
 hl.config({
-    misc = {
-        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
-	vrr = 1,
-    },
+	misc = {
+		force_default_wallpaper = -1,  -- Set to 0 or 1 to disable the anime mascot wallpapers
+		disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
+		vrr                     = 1,
+	},
+	enable_swallow = true,
+	swallow_regex = "^(kitty|Alacritty)$",
 })
 
 -- Layout
 hl.config({
-    dwindle = {
-        preserve_split = true, -- You probably want this
-    },
-    master = {
-        new_status = "master",
-    },
-    scrolling = {
-        fullscreen_on_one_column = true,
-    },
+	dwindle = {
+		preserve_split = true, -- You probably want this
+	},
+	master = {
+		new_status = "master",
+	},
+	scrolling = {
+		fullscreen_on_one_column = true,
+	},
 })
