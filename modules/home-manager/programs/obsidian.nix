f1e242524952ead2df6b602cf9a992cb29197ab6
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-04 17:50:42 +0530

    Revert "feat: configure obsidian vaults with community plugins and custom themes via nix-managed settings"
    This reverts commit 31023a70f8b059e1b3c0e391d4d18adeef0d0e90.

--- modules/home-manager/programs/obsidian.nix
+++ modules/home-manager/programs/obsidian.nix
@@ -1,32 +1,7 @@
-{ config, pkgs, lib, ... }:
-let
-  colors = config.lib.stylix.colors.withHashtag;
-  fonts = config.stylix.fonts;
-  obsidian-plugins = import ../../../pkgs/obsidian { inherit pkgs lib; };
-in
-{
-  stylix.targets.obsidian.enable = false;
+_: {
   programs.obsidian = {
     enable = true;
-    vaults."Documents/Notes" = {
-      enable = true;
-      settings = {
-        communityPlugins = with obsidian-plugins; [
-          dataview
-          templater
-          calendar
-          tasks
-          style-settings
-        ];
-        themes = with obsidian-plugins; [
-          ayu-theme
-        ];
-        app = {
-          theme = "obsidian";
-          cssTheme = "obsidian-ayu-theme";
-        };
-      };
-    };
+    vaults."Documents/Notes".enable = true;
     defaultSettings = {
       app = {
         defaultViewMode = "preview";
